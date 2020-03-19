00f7c17ddceaccf53a21005af5c6b6876534ad5f
matthewcoker <32140125+matthewcoker@users.noreply.github.com>
2020-03-19 03:41:34 -0400

    Lab5 registration functionality

---
+++ Labs/Lab5-AMAD/Lab5-AMAD/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Lab5-AMAD
+//
+//  Created by Matthew Coker on 3/18/20.
+//  Copyright © 2020 Matthew Coker. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        if userNameTextField.text != nil && passWordTextField != nil {
+            Auth.auth().createUser(withEmail: userNameTextField.text!, password: passWordTextField.text!) { (result, error) in
+                if error != nil {
+                    print("There was an error")
+                }
+                else {
+                    let uid = result?.user.uid
+                    let ref = Database.database().reference(withPath: "users").child(uid!)
+                    ref.setValue(["username" : self.userNameTextField.text!])
+                    ref.setValue(["password": self.passWordTextField.text!])
+                }
+            }
+        }
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        if userNameTextField.text != nil && passWordTextField.text != nil {
+            Auth.auth().createUser(withEmail: userNameTextField.text!, password: passWordTextField.text!) { (result, error) in
+                if error != nil {
+                    print ("Thats not good!")
+                }
+                else {
+                    //let uid = result?.user.uid
+                    
+
+                }
+            }
+        }
+    }
+    
+    
+}
+
